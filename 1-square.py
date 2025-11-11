ba5215fd26688552360dfc72168aad6b3899c0cb
dalynahgasarasi <d.gasarasi@alustudent.com>
2025-11-11 08:25:00 -0800

    Added 1-square.py

---
+++ 1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a square with private size"""
+
+    def __init__(self, size):
+        """Initialize a new Square with a private size"""
+        self.__size = size
+
